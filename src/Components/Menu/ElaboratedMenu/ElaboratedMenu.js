19da042cf733ee00fcf73ab7a5e3b39c1ccce3f2
Darshan8190 <darshankoshiya2014@gmail.com>
2020-07-09 16:24:39 -0400

    item functionality added

--- src/Components/Menu/ElaboratedMenu/ElaboratedMenu.js
+++ src/Components/Menu/ElaboratedMenu/ElaboratedMenu.js
@@ -8,24 +8,28 @@ const elaboratedMenu = (props) => (
             <h2 className={classes.heading}>Drinks</h2>
             <hr className={classes.hrLine}></hr>
             <div className={classes.inventoryWrapper}>
+
                 <div className={classes.itemBox}>
                     <div className={classes.itemFlexContainer}>
                         <div className={classes.imageBox}></div>
-                        <h3>Hot Coffees</h3>
+                        <h3 className={classes.itemHeading}>Hot Coffees</h3>
+                        <a href="/menu/drinks/hot-coffees" className={classes.itemLinkOverlay}></a>
                     </div>
                 </div>
 
                 <div className={classes.itemBox}>
                     <div className={classes.itemFlexContainer}>
                         <div className={classes.imageBox}></div>
-                        <h3>Hot Teas</h3>
+                        <h3 className={classes.itemHeading}>Hot Teas</h3>
+                        <a href="/menu/drinks/hot-teas" className={classes.itemLinkOverlay}></a>
                     </div>
                 </div>
 
                 <div className={classes.itemBox}>
                     <div className={classes.itemFlexContainer}>
                         <div className={classes.imageBox}></div>
-                        <h3>Hot Drinks</h3>
+                        <h3 className={classes.itemHeading}>Hot Drinks</h3>
+                        <a href="/menu/drinks/hot-drinks" className={classes.itemLinkOverlay}></a>
                     </div>
                 </div>
               
