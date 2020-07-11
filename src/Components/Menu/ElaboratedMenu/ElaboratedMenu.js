e54cfc6ed618b17ab6a61a16ce0b9486266856b1
Darshan8190 <darshankoshiya2014@gmail.com>
2020-07-11 13:30:11 -0400

    Indevidual section component in creation

--- src/Components/Menu/ElaboratedMenu/ElaboratedMenu.js
+++ src/Components/Menu/ElaboratedMenu/ElaboratedMenu.js
@@ -1,42 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './ElaboratedMenu.module.css'
 
-const elaboratedMenu = (props) => (
-    <div className={classes.elaboratedMenuWrapper}>
-        <h1 className={classes.menuHeading}>Menu</h1>
-        <section className={classes.sectionDrinks}>
-            <h2 className={classes.heading}>Drinks</h2>
-            <hr className={classes.hrLine}></hr>
-            <div className={classes.inventoryWrapper}>
+class ElaboratedMenu extends Component {
 
-                <div className={classes.itemBox}>
-                    <div className={classes.itemFlexContainer}>
-                        <div className={classes.imageBox}></div>
-                        <h3 className={classes.itemHeading}>Hot Coffees</h3>
-                        <a href="/menu/drinks/hot-coffees" className={classes.itemLinkOverlay}></a>
-                    </div>
-                </div>
+    state = {
+        sectionHeadings : ["Drinks", "Food", "At Home Coffee"]
+    }
 
-                <div className={classes.itemBox}>
-                    <div className={classes.itemFlexContainer}>
-                        <div className={classes.imageBox}></div>
-                        <h3 className={classes.itemHeading}>Hot Teas</h3>
-                        <a href="/menu/drinks/hot-teas" className={classes.itemLinkOverlay}></a>
-                    </div>
-                </div>
+    render() {
+        
+        let sectionHeading = ""
+        sectionHeading = this.state.sectionHeadings.map( sectionHeading => {
+            
+        })
+        return (
+            <div className={classes.elaboratedMenuWrapper}>
+                <h1 className={classes.menuHeading}>Menu</h1>
+                <section className={classes.sectionDrinks}>
+                    <h2 className={classes.heading}>Drinks</h2>
+                    <hr className={classes.hrLine}></hr>
+                    <div className={classes.inventoryWrapper}>
 
-                <div className={classes.itemBox}>
-                    <div className={classes.itemFlexContainer}>
-                        <div className={classes.imageBox}></div>
-                        <h3 className={classes.itemHeading}>Hot Drinks</h3>
-                        <a href="/menu/drinks/hot-drinks" className={classes.itemLinkOverlay}></a>
+                        <div className={classes.itemBox}>
+                            <div className={classes.itemFlexContainer}>
+                                <div className={classes.imageBox}></div>
+                                <h3 className={classes.itemHeading}>Hot Coffees</h3>
+                                <a href="/menu/drinks/hot-coffees" className={classes.itemLinkOverlay}></a>
+                            </div>
+                        </div>
+
+                        <div className={classes.itemBox}>
+                            <div className={classes.itemFlexContainer}>
+                                <div className={classes.imageBox}></div>
+                                <h3 className={classes.itemHeading}>Hot Teas</h3>
+                                <a href="/menu/drinks/hot-teas" className={classes.itemLinkOverlay}></a>
+                            </div>
+                        </div>
+
+                        <div className={classes.itemBox}>
+                            <div className={classes.itemFlexContainer}>
+                                <div className={classes.imageBox}></div>
+                                <h3 className={classes.itemHeading}>Hot Drinks</h3>
+                                <a href="/menu/drinks/hot-drinks" className={classes.itemLinkOverlay}></a>
+                            </div>
+                        </div>
+
                     </div>
-                </div>
-              
+
+                </section>
             </div>
-       
-        </section>
-    </div>
-);
+        )
+    }
+}
 
-export default elaboratedMenu;
+export default ElaboratedMenu;
